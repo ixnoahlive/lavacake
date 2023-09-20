436034693dbc8291c56ea8a512edc5cbd65c2c17
ixNoah <im.a.boiled.egg@gmail.com>
2023-09-20 09:42:01 +0100

    Added partial config functionality

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import config from './config.json' assert { type: 'json' };
+
 import { Endpoint } from './src/types/endpoint'
 import { endpoints } from './src/routes';
 
@@ -5,33 +7,40 @@ export default function Res(obj : object) {
     return new Response(JSON.stringify(obj))
 }
 
-const rateLimits: Record<string, number> = {}
+const rateLimits: Record<string, number> = {} // Track User Ratelimits
+
+// Currently unused while I'm researching how the ratelimits work
+const rateHypixel = { lastPeriod: Date.now(), requests: 0 } // Track Hypixel API Ratelimits
 Bun.serve({
     port: 9753,
     fetch(req) {
         // Check if ratelimited
-        const consumerIP = req.headers.get('x-forwarded-for');
-        if(!consumerIP) {
-            throw new Error("x-forwarded-for header is required but not provided");
+        const consumerIP = req.headers.get('x-forwarded-for') || '0.0.0.0'; // Fallback to prevnet TS annotation freakout
+        if (config.userRatelimit.enabled) {
+            if(!consumerIP) {
+                throw new Error("x-forwarded-for header is required but not provided. Disable userRatelimit in config.json to solve this.");
+            }
+            if ( rateLimits[consumerIP]>=config.userRatelimit.reqAmount ) return Res({ success: false, code: 429, error: 'You are being ratelimited' })
         }
-        if ( rateLimits[consumerIP]>=10 ) return Res({ success: false, code: 429, error: 'You are being ratelimited' })
 
         // Format data
         const reqUrl = new URL(req.url)
         const EndpointData : Endpoint = endpoints[reqUrl.pathname]
 
         // Check for valid arguments
-        if ( !endpointData ) return Res({ success: false, code: 400, error: 'Invalid pathname' })
-        if ( !endpointData.params.every( key => reqUrl.searchParams.has(key) ) ) return Res({ success: false, code: 400, error: 'Missing parameters', required: endpointData.params })
+        if ( !EndpointData ) return Res({ success: false, code: 400, error: 'Invalid pathname' })
+        if ( !EndpointData.params.every( key => reqUrl.searchParams.has(key) ) ) return Res({ success: false, code: 400, error: 'Missing parameters', required: EndpointData.params })
 
         // Manage ratelimit addition
-        if (!rateLimits[consumerIP]) rateLimits[consumerIP] = 0
-        rateLimits[consumerIP]++
-        setTimeout(() => {
-            rateLimits[consumerIP] -= 1
-        }, 10*1000);
+        if (config.userRatelimit.enabled) {
+            if (!rateLimits[consumerIP]) rateLimits[consumerIP] = 0
+            rateLimits[consumerIP]++
+            setTimeout(() => {
+                rateLimits[consumerIP] -= 1
+            }, config.userRatelimit.expires*1000);
+        }
 
         // Run endpoint code
-        return endpointData.run( req, reqUrl.searchParams )
+        return EndpointData.run( req, reqUrl.searchParams )
     }
 })
