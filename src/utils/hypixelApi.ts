92f6d5114cf5b87053226bb4f82e4ab488c1918a
ixNoah <im.a.boiled.egg@gmail.com>
2023-09-23 20:30:18 +0100

    🐔 Failsafe for users omitting token.json

--- src/utils/hypixelApi.ts
+++ src/utils/hypixelApi.ts
@@ -5,11 +5,12 @@ const tokens = tokensJSON as {
     appToken: string
 }
 
+if (tokens[tokens.active]) console.warn('You didn\'t make a valid token.json file! Please refer to README.md!')
+
 function get(endpoint: string) {
     return fetch(`https://api.hypixel.net` + endpoint, {
         method: 'GET',
         headers: { 'API-Key': tokens[tokens.active] },
     })
 }
-
 export default { get }
