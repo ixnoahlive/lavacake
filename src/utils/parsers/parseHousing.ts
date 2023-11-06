c626c14d5390431f15befb5431d045371bc21b37
ixNoah <im.a.boiled.egg@gmail.com>
2023-11-06 15:34:07 +0000

    🍪 Cookies!

--- src/utils/parsers/parseHousing.ts
+++ src/utils/parsers/parseHousing.ts
@@ -2,11 +2,13 @@ import { RankPriority } from '../../types/priority'
 
 type HousingMeta = {
     packages?: Array<string>
-    purchasedSlots?: number
+    purchasedSlots?: number,
     plotSize?: string
 }
 
 export default function parseHousing(rank: keyof typeof RankPriority, housingMeta: HousingMeta) {
+
+    // Slot Calculation
     const rankPriority = RankPriority[rank] || 0
 
     let slots = 0
@@ -19,6 +21,21 @@ export default function parseHousing(rank: keyof typeof RankPriority, housingMet
     slots += slotsDefault
     slots += slotsPurchased
 
+    // Cookie Data calculation
+    const Cookies = {}
+    const cookieKeys = Object.keys(housingMeta).filter(key => key.startsWith('given_cookies_'))
+
+    // FIXME: code could use some cleanup & look from other future contributors. cheers.
+    cookieKeys.forEach(key => {
+        const weirdDate = parseInt( /given_cookies_([0-9]+)/.exec(key)[1] )
+        
+        const year = Math.floor(weirdDate / 52)
+        const week = Math.floor((weirdDate / 52 % 1).toFixed(4) * 52) + 1 // Prevent floating point issues by using toFixed
+
+        if (!Cookies[year]) Cookies[year] = {}
+        Cookies[year][week] = { ids: housingMeta[key], spent: housingMeta[key].length }
+    })
+
     return {
         housingPlus: housingMeta.packages?.includes('housing_plus'),
 
@@ -27,5 +44,7 @@ export default function parseHousing(rank: keyof typeof RankPriority, housingMet
         slotsDefault,
 
         plotSize: housingMeta.plotSize || 'TINY',
+
+        cookies: Cookies,
     }
 }
